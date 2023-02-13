835f362a215f89b1b9756330d7a7b85433805874
Vu_Thanh_Tung <112839717+myphuong133@users.noreply.github.com>
2023-02-13 14:09:32 +0700

    Add files via upload

--- src/hien_thi_hinh.java
+++ src/hien_thi_hinh.java
@@ -1,11 +1,16 @@
 public class hien_thi_hinh {
     public static void main(String[] args){
         String result = "";
+        int x = 1;
         for (int i =1 ; i<= 5; i++){
             result = "";
-            for (int j=6;j>i;j--){
-                result += "*  ";
+            for (int j=5;j>i;j--){
+                result += "\t";
             }
+            for (int k=0;k<x;k++){
+                result += "  * ";
+            }
+            x += 2;
             System.out.println(result);
         }
 
